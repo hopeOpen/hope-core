83e7d9367bb72c2598461f7a6ad49ddbbdf16591
leonli <leonli@wezhuiyi.com>
2022-09-21 09:08:15 +0800

    feature: 修改permission接口

--- app/controller/menu/index.ts
+++ app/controller/menu/index.ts
@@ -11,59 +11,61 @@ export default class MenuController extends Controller {
    */
   @Get("permission")
   public async getMenus() {
-    return [
-      {
-        name: "首页",
-        sign: "manage.menu.index",
-        url: "",
-      },
-      {
-        name: "管理中心",
-        sign: "manage.menu.management",
-        url: "",
-      },
-      {
-        name: "个人中心",
-        parent: "manage.menu.management",
-        sign: "manage.menu.personalCenter",
-        url: "",
-      },
-      {
-        name: "用户管理",
-        parent: "manage.menu.management",
-        sign: "manage.menu.userManagement",
-        url: "",
-      },
-      {
-        name: "角色管理",
-        parent: "manage.menu.management",
-        sign: "manage.menu.roleManagement",
-        url: "",
-      },
-      {
-        name: '学期准备',
-        sign: 'manage.menu.paper',
-        url: "",
-      },
-      {
-        name: '试卷管理',
-        parent: "manage.menu.paper",
-        sign: 'manage.menu.testPaperManage',
-        url: "",
-      },
-      {
-        name: '题目管理',
-        parent: "manage.menu.paper",
-        sign: 'manage.menu.testQuestion',
-        url: "",
-      },
-      {
-        name: '题库管理',
-        parent: "manage.menu.paper",
-        sign: 'manage.menu.questionBank',
-        url: "",
-      }
-    ];
+    const { ctx } = this;
+    return await ctx.service.menu.getMenus();
+    // return [
+    //   {
+    //     name: "首页",
+    //     sign: "manage.menu.index",
+    //     url: "",
+    //   },
+    //   {
+    //     name: "管理中心",
+    //     sign: "manage.menu.management",
+    //     url: "",
+    //   },
+    //   {
+    //     name: "个人中心",
+    //     parent: "manage.menu.management",
+    //     sign: "manage.menu.personalCenter",
+    //     url: "",
+    //   },
+    //   {
+    //     name: "用户管理",
+    //     parent: "manage.menu.management",
+    //     sign: "manage.menu.userManagement",
+    //     url: "",
+    //   },
+    //   {
+    //     name: "角色管理",
+    //     parent: "manage.menu.management",
+    //     sign: "manage.menu.roleManagement",
+    //     url: "",
+    //   },
+    //   {
+    //     name: '学期准备',
+    //     sign: 'manage.menu.paper',
+    //     url: "",
+    //   },
+    //   {
+    //     name: '试卷管理',
+    //     parent: "manage.menu.paper",
+    //     sign: 'manage.menu.testPaperManage',
+    //     url: "",
+    //   },
+    //   {
+    //     name: '题目管理',
+    //     parent: "manage.menu.paper",
+    //     sign: 'manage.menu.testQuestion',
+    //     url: "",
+    //   },
+    //   {
+    //     name: '题库管理',
+    //     parent: "manage.menu.paper",
+    //     sign: 'manage.menu.questionBank',
+    //     url: "",
+    //   }
+    // ];
   }
 
   /**
