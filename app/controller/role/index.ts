2f7e96dc1b7473760b7ce0ce6b9db9f7da21d55d
leonli <leonli@wezhuiyi.com>
2022-09-23 18:52:45 +0800

    feature: 新增角色接口

--- app/controller/role/index.ts
+++ app/controller/role/index.ts
@@ -1,4 +1,5 @@
 import { Controller } from "egg";
+import { BadRequestException } from "../../exception/badRequest.exception";
 import { Control, Post } from "../../lib/requestMapping/decorator/httpMethod.decorator";
 import { Body } from "../../lib/requestMapping/decorator/routerParams.decorator";
 
@@ -13,5 +14,42 @@ export default class RoleController extends Controller {
     const { ctx } = this;
     return await ctx.service.role.getRoles(body);
   }
+
+  /**
+   * 新增角色
+   */
+  @Post('add')
+  public async addRole(@Body() body: { roleName: string, menuConfig: number[], description: string }) {
+    const { ctx } = this;
+    const { roleName, menuConfig, description } = body;
+    if (!roleName.trim()) {
+      throw new BadRequestException('角色名称不能为空');
+    }
+    if (!menuConfig.length) {
+      throw new BadRequestException('角色权限至少有一项');
+    }
+    // 校验角色名称是否存在
+    const result = await ctx.service.role.checkRoleName(roleName);
+    if (result && result.length) {
+      throw new BadRequestException('角色名称已存在');
+    }
+    return await ctx.service.role.addRole({ roleName, menuConfig: JSON.stringify(menuConfig), description });
+  }
+
+  /**
+   * 删除角色
+   */
+  @Post('delete')
+  public async deleteRole(@Body() body: { ids: number[] }) {
+    const { ctx } = this;
+    const { ids } = body;
+    if(!ids || !ids.length) {
+      throw new BadRequestException('id is required');
+    }
+    // 判断角色是否存在
+    await ctx.service.role.checkRole(ids)
+    return await ctx.service.role.deleteRole(ids);
+  }
+
 
 }
--- app/service/role.ts
+++ app/service/role.ts
@@ -1,5 +1,6 @@
 
 import { Service } from 'egg';
+import { BadRequestException } from '../exception/badRequest.exception';
 
 export default class MenuService extends Service {
 
@@ -10,11 +11,75 @@ export default class MenuService extends Service {
   public async getRoles(body: { pageNum: number, pageSize: number }) {
     const { ctx } = this;
     const { pageSize, pageNum } = body;
-    return await ctx.model.Roles.findAll({
+    const result =  await ctx.model.Roles.findAndCountAll({
       attributes: ['id', 'roleName', 'menuConfig', 'description'],
       limit: pageSize,
       offset: (pageNum - 1) * pageSize
     });
+    return {
+      list: result.rows,
+      total: result.count,
+      pageNum,
+      pageSize
+    }
   }
 
+  /**
+   * 新增角色
+   */
+  public async addRole(body: { roleName: string, menuConfig: string, description: string }) {
+    const { ctx } = this;
+    const result = await ctx.model.Roles.create(body);
+    if (!result) {
+      throw new Error('新增角色失败');
+    }
+    return result;
+  }
+  
+  /**
+   * 校验角色名称是否存在
+   */
+  public async checkRoleName(roleName: string) {
+    return await this.ctx.model.Roles.findAll({
+      where: {
+        roleName
+      }
+    });
+  }
+
+  /**
+   * 校验角色是否存在
+   */
+  public async checkRole(id: number[]) {
+    const { ctx } = this;
+    const result = await ctx.model.Roles.findAll({
+      where: {
+        id,
+      },
+    });
+    if (!result) {
+      throw new BadRequestException('角色不存在');
+    }
+    // 比较id长度是否一致
+    if(result.length !== id.length) {
+      throw new BadRequestException('包含不存在的角色');
+    }
+  }
+
+  /**
+   * 删除角色
+   * TODO：删除角色时，需要判断角色是否被使用
+   */
+  public async deleteRole(id: number[]) {
+    const { ctx } = this;
+    const result = await ctx.model.Roles.destroy({
+      where: {
+        id,
+      },
+    });
+    if (!result) {
+      throw new BadRequestException('删除角色失败');
+    }
+    return result;
+  }
 }
